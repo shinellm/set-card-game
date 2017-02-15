faefed7832e627bb9b8d0fa2794ae5d950670377
ELIZABETH-PC\Elizabeth <elizabethr>
2017-02-14 21:44:22 -0500

    Added Tutorial Class with method stubs.

---
+++ Set/src/Tutorial.java
@@ -0,0 +1,56 @@
+/**
+ * Tutorial Mode
+ * 
+ * @author Elizabeth
+ * @see Mode.java
+ */
+public class Tutorial extends Mode {
+
+	public Drawing dwg;
+	public Deck deck;
+	public Graphics page;
+	private Command cmd;
+	private int muliIndex;
+	
+	/**
+	 * Constructor 
+	 * @param page
+	 * @param dwg
+	 * @param deck
+	 */
+	public Tutorial(Graphics page, Drawing dwg, Deck deck){
+		
+	}
+	
+	/**
+	 * Goes to next set
+	 */
+	public void forward(){
+		
+	}
+	
+	/**
+	 * Goes to previous set
+	 */
+	public void backward(){
+		
+	}
+	
+	/**
+	 * Adds next 12 cards to playing table
+	 */
+	public void newCards(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void paintComponent(){
+		
+	}
+	
+	private void actionPerformed(){
+		
+	}
+}
