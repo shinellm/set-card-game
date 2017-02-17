62ef7835437d4c330bc092867f689f34c8631d6f
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-17 10:45:01 -0500

    Created a Command.java class and added the method stub.

---
+++ Set/src/Command.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+/**
+ * Command.java
+ * Superclass for commands.
+ * Provides empty definitions for the executeClick method.
+ * 
+ * @author Shinell Manwaring
+ */
+public class Command {
+  public void executeClick(Point p, Drawing dwg) { }
+}
+
