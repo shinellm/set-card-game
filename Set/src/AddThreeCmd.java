06bf21393f87c8309ac8bfdc3a20866ec25fa6a1
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-17 10:57:01 -0500

    Created a AddThreeCmd.java class and added the agreed upon method stubs.

---
+++ Set/src/AddThreeCmd.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+/**
+ * AddThreeCmd.java
+ * Command class to perform an add three cards command.
+ * 
+ * @author Shinell Manwaring
+ * @see Command
+ */
+
+public class AddThreeCmd extends Command {
+  
+  /**
+   * When the "Add Three Cards" button is clicked, draw three cards from
+   * the deck and add it to the cards at play.
+   * 
+   * @param dwg the drawing 
+   */
+  public void executeClick(Drawing dwg) {
+	  
+	  
+  }
+}
