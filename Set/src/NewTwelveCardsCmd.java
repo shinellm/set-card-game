07b10abae17b9a9ec552af458fc90a74a82c77be
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-17 11:09:14 -0500

    Created a NewTwelveCardsCmd.java class and added the agreed upon method stubs.

---
+++ Set/src/NewTwelveCardsCmd.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+/**
+ * NewTwelveCardsCmd.java
+ * Command class to perform a add twelve new cards command.
+ * 
+ * @author Shinell Manwaring
+ * @see Command
+ */
+
+public class NewTwelveCardsCmd extends Command {
+  
+  /**
+   * When the "New Twelve Cards" button is clicked, replace the
+   * cards at play with twelve new cards.
+   * 
+   * @param dwg the drawing 
+   */
+  public void executeClick(Drawing dwg) {
+	  
+	  
+  }
+}
