be263c21e759444e6691ac1be7cad4a68f8391f7
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-17 11:04:25 -0500

    Created a HintCmd.java class and added the agreed upon method stubs.

---
+++ Set/src/HintCmd.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+/**
+ * HintCmd.java
+ * Command class to perform a hint command.
+ * 
+ * @author Shinell Manwaring
+ * @see Command
+ */
+
+public class HintCmd extends Command {
+  
+  /**
+   * When the "Hint" button is clicked, check the cards at play
+   * and determine if there is a set. If there is a set, highlight
+   * the cards that make up that set and display "There are <number of sets>
+   * sets found". If there are no sets, display the message "There are
+   * zero sets found."
+   * 
+   * @param dwg the drawing 
+   */
+  public void executeClick(Drawing dwg) {
+	  
+	  
+  }
+}
