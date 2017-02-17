bf4a5e013156d7271934a2fd453e93e1fc6a35f0
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-17 11:29:50 -0500

    Created a RestartCmd.java class and added the agreed upon method stubs.

---
+++ Set/src/RestartCmd.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+/**
+ * RestartCmd.java
+ * Command class to perform a restart command.
+ * 
+ * @author Shinell Manwaring
+ * @see Command
+ */
+
+public class RestartCmd extends Command {
+  
+  /**
+   * When the "Restart" button is clicked, restart the mode the user is
+   * currently in. Shuffle the cards in the deck and have the decks pointer 
+   * return to index 0.  
+   * 
+   * @param dwg the drawing 
+   */
+  public void executeClick(Drawing dwg) {
+	  
+	  
+  }
+}
