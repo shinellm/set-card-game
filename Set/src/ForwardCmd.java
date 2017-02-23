38358b2b8c6ac122be298bc043bfe85ec7d0fa49
Shinell Manwaring <Yoshi@Shinell-Manwarings-MacBook-Pro.local>
2017-02-23 18:31:52 -0500

    Fixed the ForwardCmd.java so that it unlightlights the previous set shown

--- Set/src/ForwardCmd.java
+++ Set/src/ForwardCmd.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 public class ForwardCmd extends Command {
 	private ArrayList<ArrayList<Card>> sets; // The three cards, which make a set
+	private ArrayList<Card> setToDeselect; //The last three cards highlighted
 
 	/**
 	 * When the forward arrow button is clicked, display another set that exists
@@ -21,11 +22,21 @@ public class ForwardCmd extends Command {
 	 */
 	public void executeClick(Drawing dwg) {
 		sets = dwg.getAllSets();
-		
 		if (dwg.getHighlight() != sets.size()) {
 			ArrayList<Card> setToHighlight = sets.get(dwg.getHighlight());
+			
 			for (int i = 0; i < 3; i++) { //Use the multi-dimensional ArrayList received to highlight only one index
 				setToHighlight.get(i).setHighlighted();
+			}
+			
+			if (dwg.getHighlight()-1 >= 0) {
+				setToDeselect = sets.get(dwg.getHighlight()-1);
+				
+				for (int i = 0; i < 3; i++) { //Deselect the previously highlighted cards
+					if (setToDeselect.get(i) != setToHighlight.get(i)) {
+					setToDeselect.get(i).unsetHighlighted();	
+					}
+				}
 			}
 			dwg.incrementHighlight(); //Increments the index I took the cards out of and keeps track of the new index
 		}
